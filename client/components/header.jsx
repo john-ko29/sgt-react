7fef22b6ecf6c347c2970ef57e3fe3ee96380e64
John Ko <jko121790@gmail.com>
2020-04-17 23:18:33 -0700

    created average grade in header

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,6 +4,7 @@ function Header(props) {
   return (
     <div className="header d-flex justify-content-around align-items-center">
       <h1>Student Grade Table</h1>
+      <h3>Average Grade <span className='badge badge-secondary'>{props.average}</span></h3>
     </div>
   );
 }
